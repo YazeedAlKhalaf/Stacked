8a446a92da2d2a516218aeb60d0056389972a88b
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-06-08 21:53:32 +0300

    :bug: Code Refactor

--- README.md
+++ README.md
@@ -1,19 +1,12 @@
 # Stacked VS Code Extension
 
-Snippets:
-
-1. `svrf` => Stacked View Reactive Statefull
-2. `svnf` => Stacked View NonReactive Statefull
-3. `svrl` => Stacked View Reactive Stateless
-4. `svnl` => Stacked View NonReactive Stateless
-
 ## Commands Usage
 
-1. Stacked: New Stacked
+1. `Stacked: New Stacked View ViewModel`
    - This Command generates for you everything you need to create your stacked folder.
    - You can use this command in two ways:
-     1. Click `Ctrl+Shift+P` then search for `Stacked: New Stacked`
-     2. Right click on the `views` folder, then click on `Stacked: New Stacked`
+     1. Click `Ctrl+Shift+P` then search for `Stacked: New Stacked View ViewModel`
+     2. Right click on the `views` folder, then click on `Stacked: New Stacked View ViewModel`
 
 ## Snippets Usage
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,9 @@
-import * as lodash from "lodash";
-import { commands, ExtensionContext, Uri } from "vscode";
-import { lstatSync } from "fs";
-import {
-  promptForStackedName,
-  promptForTargetDirectory,
-  promptForUseReactive,
-  generateStackedCode,
-  convertToPascalCase,
-  showErrorMessage,
-  showInformationMessage,
-} from "./utils";
+import { commands, ExtensionContext } from "vscode";
+import { newStackedViewViewModelCommand } from "./commands";
 
 export function activate(_context: ExtensionContext) {
-  commands.registerCommand("extension.new-stacked", async (uri: Uri) => {
-    const stackedName = await promptForStackedName();
-    if (lodash.isNil(stackedName) || stackedName.trim() === "") {
-      showErrorMessage("The stacked name must not be empty");
-      return;
-    }
-
-    let targetDirectory;
-    if (
-      lodash.isNil(lodash.get(uri, "fsPath")) ||
-      !lstatSync(uri.fsPath).isDirectory()
-    ) {
-      targetDirectory = await promptForTargetDirectory();
-      if (lodash.isNil(targetDirectory)) {
-        showErrorMessage("Please select a valid directory");
-        return;
-      }
-    } else {
-      targetDirectory = uri.fsPath;
-    }
-
-    const useReactive = (await promptForUseReactive()) === "yes (default)";
-
-    const pascalCaseStackedName = convertToPascalCase(
-      stackedName.toLowerCase()
-    );
-
-    try {
-      await generateStackedCode(stackedName, targetDirectory, useReactive);
-      showInformationMessage(
-        `Successfully Generated ${pascalCaseStackedName} Stacked`
-      );
-    } catch (error) {
-      showErrorMessage(`
-      Error:
-      ${error instanceof Error ? error.message : JSON.stringify(error)}
-      `);
-    }
-  });
+  commands.registerCommand(
+    "extension.new-stacked-view-viewmodel",
+    newStackedViewViewModelCommand
+  );
 }
